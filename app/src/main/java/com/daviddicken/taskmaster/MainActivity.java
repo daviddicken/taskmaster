809113649a5eeba6afd4a2d5f7e02c080d263fcd
daviddicken <davidfromseattle@gmail.com>
2020-10-30 00:19:00 -0700

    refactored addtask page too

--- app/src/main/java/com/daviddicken/taskmaster/MainActivity.java
+++ app/src/main/java/com/daviddicken/taskmaster/MainActivity.java
@@ -44,65 +44,39 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnInt
     @Override
     public void onResume(){
         super.onResume();
-        preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-//        ArrayList<Team> teams = new ArrayList<>();
-//
-//        //========== Get Teams ========================
-//        Amplify.API.query(ModelQuery.list(Team.class),
-//                response -> {
-//                    for(Team team : response.getData()){
-//                        teams.add(team);
-//                    } },
-//                error -> Log.e("AmplifyAddTask", "failed getting teams"));
-
-        // check for and display user name
-        TextView name = findViewById(R.id.theName);
-        String namePassedIn = preferences.getString("usersName", "No User Entered");
-        name.setText(String.format("%s's tasks:", namePassedIn));
-        String userTeam = preferences.getString("userTeam", "No team selected");
-
-
+        populateViews();
         populateRecycler();
-
-//        Team chosenTeam = null;
-//        for(int i = 0; i < teams.size(); i++) {
-//            if (teams.get(i).getName().equals(userTeam)) {
-//                chosenTeam = teams.get(i);
-//            }
-//        }
     }
 
+//=================== ON CREATE ==================================
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //tasks.clear();
         setupRecyclerView();
         configAws();
         setupButtons();
         createHandler();
         populateRecycler();
+        populateViews();
+    }
 
+    //============= Go to Task Detail Page =================
+    @Override
+    public void taskListener(Task task) {
+        Intent intent = new Intent(MainActivity.this, TaskDetail.class);
 
+        intent.putExtra("taskTitle", task.getTitle());
+        intent.putExtra("taskBody", task.getDescription());
+        intent.putExtra("taskState", task.getStatus());
+        this.startActivity(intent);
+    }
 
-//=====================================================
-//        public void configAws() {
-//            // initialize Amplify API
-//            try {
-//                Amplify.addPlugin(new AWSApiPlugin());
-//                Amplify.configure(getApplicationContext());
-//                //createTeams();
-//
-//            } catch (AmplifyException e) {
-//                Log.e("MainActivityAmplify", "Could not initialize Amplify", e);
-//            }
-//        }
-//==================================================
-        //ArrayList<Task> taskDb = (ArrayList<Task>) database.taskDao().getDbTasks(); // TODO check name
-
+    //============ Populate views (Users name) ====
+    public void populateViews() {
         // setup to be able to save and access data to and from SharedPreferences (local storage)
         preferences = PreferenceManager.getDefaultSharedPreferences(this); // getter
         final SharedPreferences.Editor preferencesEditor = preferences.edit();
@@ -112,97 +86,6 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnInt
         String namePassedIn = preferences.getString("usersName", "No User Entered");
 
         name.setText(String.format("%s's tasks:", namePassedIn));
-
-        //================ database stuff ============
-        // database build
-//        database = Room.databaseBuilder(getApplicationContext(), Database.class, "dbBucket")
-//                //.fallbackToDestructiveMigration()
-//                .allowMainThreadQueries()
-//                .build();
-
-        //======== RecyclerView =========================
-        //tasks =new ArrayList<>(); //TODO: is this needed anymore
-
-//        public void setupRecyclerView() {
-//            RecyclerView recyclerView = findViewById(R.id.recyclerTaskList);
-//            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//            recyclerView.setAdapter(new TaskAdapter(tasks, this));
-//        }
-
-//==========================================================================
-//        public void createHandler() {
-//            handler = new Handler(Looper.getMainLooper(),
-//                    new Handler.Callback() {
-//                        @Override
-//                        public boolean handleMessage(@NonNull Message msg) {
-//                            recyclerView.getAdapter().notifyDataSetChanged();
-//                            return false;
-//                        }
-//                    });
-//        }
-//===============================================================
-//        public void populateRecycler() {
-//            Amplify.API.query(
-//                    ModelQuery.list(Task.class),
-//                    response -> {
-//                        for (Task task : response.getData()) {  //populate list for recyclerview.
-//                            if (preferences.contains("userTeam")) { // check that a user team exist before trying to find teams task
-//                                if (task.taskForTeam.getName().equals(preferences.getString("userTeam", " "))) {
-//                                    tasks.add(task);
-//                                }
-//                            } else {
-//                                tasks.add(task);
-//                            }
-//
-//                        }
-//                        handler.sendEmptyMessage(1);
-//                        Log.i("AmplifyQuery", "Number of items from dynamodb" + tasks.size());
-//                    },
-//                    error -> Log.i("AmplifyQuery", "Failed to get items"));
-//        }
-
-//        //================= Buttons =====================
-//        public void setupButtons() {
-//            // got to settings activity
-//            ImageButton settingsActivity = MainActivity.this.findViewById(R.id.goToSettings);
-//            settingsActivity.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent settingsIntent = new Intent(MainActivity.this, Settings.class);
-//                    MainActivity.this.startActivity(settingsIntent);
-//                }
-//            });
-//
-//            // add Task button
-//            Button addTask = MainActivity.this.findViewById(R.id.addTask);
-//            addTask.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent addTaskIntent = new Intent(MainActivity.this, AddATask.class);
-//                    MainActivity.this.startActivity(addTaskIntent);
-//                }
-//            });
-//
-//            // go to all tasks button
-//            Button allTask = MainActivity.this.findViewById(R.id.allTasks);
-//            allTask.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent allTaskIntent = new Intent(MainActivity.this, AllTasks.class);
-//                    MainActivity.this.startActivity(allTaskIntent);
-//                }
-//            });
-//        }
-    }
-
-    @Override
-    public void taskListener(Task task) {
-        Intent intent = new Intent(MainActivity.this, TaskDetail.class);
-
-        intent.putExtra("taskTitle", task.getTitle());
-        intent.putExtra("taskBody", task.getDescription());
-        intent.putExtra("taskState", task.getStatus());
-        this.startActivity(intent);
     }
 
     //============= RecyclerView ===========================
@@ -316,3 +199,16 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnInt
                 error -> Log.e("Amplify", "Team created"));
     }
 }// end of mainActivity
+
+
+
+//==================== Zombie code =======================
+
+//ArrayList<Task> taskDb = (ArrayList<Task>) database.taskDao().getDbTasks();
+
+//================ database stuff ============
+// database build
+//        database = Room.databaseBuilder(getApplicationContext(), Database.class, "dbBucket")
+//                //.fallbackToDestructiveMigration()
+//                .allowMainThreadQueries()
+//                .build();
